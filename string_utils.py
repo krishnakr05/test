e7f0efe44a00d1b0f9895667d913da88763bfc3c
Krishna K R <158593908+krishnakr05@users.noreply.github.com>
2026-07-20 00:20:54 +0530

    Add docstrings for string utility functions

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,8 @@
+"""Reverses the given string."""
 def reverse_string(s):
     return s[::-1]
 
+"""Counts the number of vowels in a string."""
 def count_vowels(s):
     count = 0
     for char in s.lower():
